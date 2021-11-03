182942011f75e6c5e7313b78fceed8a3ce231a9b
joshinils <joshua.laskowski@web.de>
2021-11-03 11:54:44 +0100

    add function to geenerate variations for the same point in time as list of strings

--- debug_print.py
+++ debug_print.py
@@ -1,10 +1,13 @@
 import inspect
 
-def debug_print(elem):
+def debug_print(*argv):
+    elem = ""
+    for arg in argv:
+        elem += str(type(arg)) + ": \"" + str(arg) + "\", "
     info = ""
     info += inspect.currentframe().f_back.f_code.co_filename
     info += ":"
     info += str(inspect.currentframe().f_back.f_lineno)
     info += " "
     info += "(" + inspect.currentframe().f_back.f_code.co_name + ")"
-    print(info, str(type(elem)) + ": \"" + str(elem) + "\"")
+    print(info, elem)
--- test_erinnerung.py
+++ test_erinnerung.py
@@ -8,7 +8,101 @@ from debug_print import *
 
 import datetime
 from datetime import date, timedelta
+import itertools
+import typing 
 
+def get_arg_list(arg: int) -> typing.List[str]:
+    # return [str(arg)]  # debug
+    arg_str = str(arg)
+    arg_str_list = list(set([arg_str, arg_str.zfill(2)]))
+    arg_str_list.sort(reverse=True)
+    return arg_str_list
+
+
+def test_string_list(weeks=0, days=0, hours=0, minutes=0, seconds=0):
+    separator = [":", ",", ";", "."]
+    # separator = [":"]  # debug
+    space = ["", " "]
+    # space = [""]  # debug
+    separator = [''.join(i) for i in itertools.product(space, separator, space)]
+
+    # print(weeks, days, hours, minutes, seconds) # debug
+    ret = [""]
+
+    if (weeks == 0):
+        arg_weeks = None
+    else:
+        arg_weeks = get_arg_list(weeks)
+        if days == 0 and hours == 0 and minutes == 0 and seconds == 0:
+            week_name = ["w", "woche", "wochen", "week", "weeks"]
+            # week_name = ["w"]  # debug
+            ret = [''.join(i) for i in itertools.product(arg_weeks, space, week_name)]
+        else:
+            ret = [''.join(i) for i in itertools.product(arg_weeks, separator)]
+
+    # rest is 0, return early
+    if (days == 0 and hours == 0 and minutes == 0 and seconds == 0):
+        return ret
+
+    if (days == 0 and weeks == 0):
+        arg_days = None
+    else:
+        arg_days = get_arg_list(days)
+        if hours == 0 and minutes == 0 and seconds == 0:
+            day_name = ["d", "day", "days", "t", "tag", "tage"]
+            # day_name = ["d"]  # debug
+            arg_days = [''.join(i) for i in itertools.product(arg_days, space, day_name)]
+            ret = [''.join(i) for i in itertools.product(ret, arg_days)]
+        else:
+            ret = [''.join(i) for i in itertools.product(ret, arg_days, separator)]
+
+    # rest is 0, return early
+    if (hours == 0 and minutes == 0 and seconds == 0):
+        return ret
+
+    if (hours == 0 and days == 0 and weeks == 0):
+        arg_hours = None
+    else:
+        arg_hours = get_arg_list(hours)
+        if minutes == 0 and seconds == 0:
+            hour_name = ["h", "hour", "hours", "stunde", "stunden"]
+            # hour_name = ["h"]  # debug
+            arg_hours = [''.join(i) for i in itertools.product(arg_hours, space, hour_name)]
+            ret = [''.join(i) for i in itertools.product(ret, arg_hours)]
+        else:
+            ret = [''.join(i) for i in itertools.product(ret, arg_hours, separator)]
+
+    # rest is 0, return early
+    if (minutes == 0 and seconds == 0):
+        return ret
+
+    if (minutes == 0 and hours == 0 and days == 0 and weeks == 0):
+        arg_minutes = None
+    else:
+        arg_minutes = get_arg_list(minutes)
+        if seconds == 0:
+            minute_name = ["m", "minute", "minutes", "minuten"]
+            # minute_name = ["m"]  # debug
+            arg_minutes = [''.join(i) for i in itertools.product(arg_minutes, space, minute_name)]
+            ret = [''.join(i) for i in itertools.product(ret, arg_minutes)]
+        else:
+            ret = [''.join(i) for i in itertools.product(ret, arg_minutes, separator)]
+
+    # rest is 0, return early
+    if (seconds == 0):
+        return ret
+
+    if (seconds == 0 and hours == 0 and hours == 0 and days == 0 and weeks == 0):
+        arg_seconds = None
+    else:
+        arg_seconds = get_arg_list(seconds)
+
+        second_name = ["s", "sec", "sek", "second", "seconds", "sekunden"]
+        # second_name = ["s"]  # debug
+        arg_seconds = [''.join(i) for i in itertools.product(arg_seconds, space, second_name)]
+        ret = [''.join(i) for i in itertools.product(ret, arg_seconds)]
+
+    return ret
 
 def split_list(l: list) -> list:
     new_l = []
