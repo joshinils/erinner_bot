a2be32906eed5b2b9dd78138e24a927c5c551d66
joshinils <joshua.laskowski@web.de>
2023-01-11 16:08:37 +0100

    add putzen bulk script

---
+++ putzen.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+./t_msg "papier kleinmachen, rausbringen"
+./t_msg "küche papiermüll rausbringen"
+./t_msg "küche restmüll rausbringen"
+./t_msg "küche wertstoffmüll rausbringen"
+./t_msg "küche biomüll rausbringen"
+
+./t_msg "küche pfand weg"
+./t_msg "küche eistee hoch?"
+
+./t_msg "küche herd putzen"
+./t_msg "küche spüle putzen"
+./t_msg "EG bad toilette reinigen"
+
+./t_msg "EG bad saugen"
+./t_msg "küche saugen"
+./t_msg "EG saugen"
+
+./t_msg "EG bad boden wischen"
+./t_msg "küche boden wischen"
+
+./t_msg "OG bad toilette reinigen"
+./t_msg "OG bad badewanne reinigen"
+./t_msg "OG bad waschbecken reinigen"
+./t_msg "OG bad saugen"
+./t_msg "OG bad boden wischen"
+
+./t_msg "OG müll unter treppe raus"
+./t_msg "OG müll petra papier raus"
+./t_msg "OG müll petra plastik raus"
+./t_msg "OG müll petra bett raus"
+./t_msg "OG müll joshua raus"
+
+./t_msg "OG saugen"
